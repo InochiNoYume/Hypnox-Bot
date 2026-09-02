317e164bd70d72ceca7aebf118659758bf9a4736
Elias Ibañez Uribe <eibanezu2024@alumnos.ceat.cl>
2026-09-01 21:55:45 -0400

    fix: restrict community links command by role ID

--- src/commands/links.js
+++ src/commands/links.js
@@ -27,5 +27,8 @@ module.exports = {
       .setTimestamp();
 
     return interaction.reply({ embeds: [embed] });
-  }
+  },
+
+  guilds: ['official'],
+  access: { roleEnvs: ['OFFICIAL_ROLE_MEMBER_ID'] }
 };
