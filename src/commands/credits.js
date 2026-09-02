32d9458ea85de2e174b838a25de45d9d88d8a263
Elias Ibañez Uribe <eibanezu2024@alumnos.ceat.cl>
2026-09-01 20:14:25 -0400

    feat: add credits command

---
+++ src/commands/credits.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { baseEmbed } = require('../utils/embeds');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('credits')
+    .setDescription('Muestra los créditos de Hypnox Studios.'),
+
+  async execute(interaction) {
+    const embed = baseEmbed(
+      'CRÉDITOS',
+      'Hypnox Studios\n\nProyecto, comunidad y bot desarrollados para centralizar la gestión de nuestros servidores de Discord.'
+    );
+
+    await interaction.reply({ embeds: [embed] });
+  }
+};
