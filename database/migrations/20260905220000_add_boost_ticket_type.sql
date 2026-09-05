324f3a77d220bd6a440e7ffbc7270431c9858d0e
Elias Ibañez Uribe <eibanezu2024@alumnos.ceat.cl>
2026-09-05 18:18:54 -0400

    db: remove duplicate boost migration filename

--- database/migrations/20260905220000_add_boost_ticket_type.sql
+++
@@ -1,5 +0,0 @@
-alter table public.tickets drop constraint if exists tickets_ticket_type_check;
-
-alter table public.tickets
-  add constraint tickets_ticket_type_check
-  check (ticket_type = any (array['support'::text, 'report'::text, 'alliance_partner'::text, 'contact'::text, 'bugs'::text, 'boost'::text]));
